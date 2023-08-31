695448010454dc16fdacb28af566ef606a8967e2
MuhammadAhsan786 <solutionweb386@gmail.com>
2023-08-31 20:10:24 +0500

    custom changes

---
+++ pages/DashboardMain/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SidebarWithHeader from '../../src/components/app/Flok/DashboardUser/UserDahboard'
+
+
+function index() {
+  return (
+    <>
+    <SidebarWithHeader/>
+    </>
+  )
+}
+
+export default index
